d83963716609ae4ca3d4dbe5de6907b3b6f6ef76
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2025-10-01 23:45:59 -0400

    added data collection for ERA5

--- code/01_ingest_clean/main_ingest.py
+++ code/01_ingest_clean/main_ingest.py
@@ -1,14 +1,19 @@
 import sys
+import os
 sys.path.insert(0, '99_utils')
 import utils
 import objects
 
 data_raw_folder = objects.DATA_RAW
+data_processed  = 
 peru_adm3_url   = objects.PERU_ADM3_GEOJSON
 
 def main():
 
     utils.download_save(peru_adm3_url, data_raw_folder)
+    #utils.era5_data()
+    #os.rename('269c15be7865b6c32e03872748bb00d7.grib', 
+    #    f'{data_raw_folder}/269c15be7865b6c32e03872748bb00d7.grib')
 
 
 if __name__ == '__main__':
--- code/99_utils/utils.py
+++ code/99_utils/utils.py
@@ -1,3 +1,4 @@
+import cdsapi
 import objects
 import requests
 
@@ -15,3 +16,38 @@ def download_save(url, path):
             f.write(chunk)
 
     return True
+
+def era5_data():
+
+    dataset = "reanalysis-era5-single-levels"
+    request = {
+    "product_type": ["reanalysis"],
+    "variable": ["2m_temperature"],
+    "year": ["2022", "2023", "2024"],
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": ["00:00", "12:00"],
+    "data_format": "grib",
+    "download_format": "unarchived",
+    "area": [-0.03860596799995619, -81.32823048999995, -18.35092773599996, -68.65227910299996]
+}
+
+    client = cdsapi.Client()
+    client.retrieve(dataset, request).download()

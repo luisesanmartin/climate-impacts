b51e0ba13504db5bc472c3f9ddfb0d3eadc74a5e
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2025-10-01 23:13:39 -0400

    corrected sinadef years to 2022-2024

--- code/02_feature_engineering/sinadef-collapse.R
+++ code/02_feature_engineering/sinadef-collapse.R
@@ -6,9 +6,10 @@ df <- read_csv(here('data_raw', 'SINADEF_DATOS_ABIERTOS.csv')) %>%
   janitor::clean_names()
 
 df2 <- df %>%
-  filter(pais_domicilio == 'PERU') %>%
-  filter(cod_ubigeo_domicilio != 'SIN REGISTRO' &
-           distrito_domicilio != 'SIN REGISTRO') %>%
+  filter(pais_domicilio == 'PERU' &
+           cod_ubigeo_domicilio != 'SIN REGISTRO' &
+           distrito_domicilio != 'SIN REGISTRO' &
+           anio %in% 2022:2024) %>%
   select(year = anio,
          ubigeo = cod_ubigeo_domicilio) %>%
   mutate(death = 1,

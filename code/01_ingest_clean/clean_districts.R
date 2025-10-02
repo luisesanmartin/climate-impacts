39e21a598de3b1eb3237f2723eb4ae482a48d44d
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2025-10-01 23:13:19 -0400

    script to clean districts

---
+++ code/01_ingest_clean/clean_districts.R
@@ -0,0 +1,19 @@
+library(sf)
+library(here)
+
+temp <- tempfile()
+
+unzip(zipfile = here('data_raw', 'per_adm_ign_20200714_shp.zip'),
+      exdir = temp)
+
+df <- read_sf(temp, layer='per_admbnda_adm3_ign_20200714') %>%
+  janitor::clean_names()
+
+df2 <- df%>%
+  select(ubigeo = adm3_pcode,
+         dpto = adm1_es,
+         prov = adm2_es,
+         dist = adm3_es) %>%
+  mutate(ubigeo = substr(ubigeo, 3, 8))
+
+st_write(df2, here('data_processed', 'peru_districts.geojson'))

d82f059c63e3d9bfd06403c161520a14bec4aeea
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2025-10-03 02:22:05 -0400

    district population data wrangling

---
+++ code/02_feature_engineering/district_population.R
@@ -0,0 +1,21 @@
+library(here)
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+path <- here('data_raw', 'reporte_estadist.xlsx')
+df <- read_excel(path) %>% 
+  janitor::clean_names() %>%
+  filter(!is.na(ubigeo)) %>%
+  select(ubigeo,
+         poblacion2022 = x11,
+         poblacion2023 = x12,
+         poblacion2024 = x13) %>%
+  pivot_longer(
+    cols = starts_with("poblacion"),
+    names_to = "anio",
+    values_to = "poblacion"
+  ) %>%
+  mutate(anio = gsub("poblacion", "", anio))
+
+saveRDS(df, file = here('data_processed', 'analysis', 'district-year-population.rds'))
